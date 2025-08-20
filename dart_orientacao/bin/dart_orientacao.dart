fe8968018efe699cafbfa47d35217d681ac98ad2
Vinicius <viniciustomazi0712@gmail.com>
2025-08-19 21:29:04 -0300

    Adicionado NPC Herdado da herança

--- dart_orientacao/bin/dart_orientacao.dart
+++ dart_orientacao/bin/dart_orientacao.dart
@@ -1,6 +1,7 @@
 import 'character.dart';
 import 'naruto_character.dart';
 import 'gta_character.dart';
+import 'npcGTA_character.dart';
 void main(){
   /// Tio Patinhas
   Character scroogeMcDuck = Character(name: 'Tio Patinhas', universe: 'Duck Tales', mainSkill: 'Riqueza', childrenCount: 0, isProtagonist: true);
@@ -13,7 +14,11 @@ void main(){
   /// Franklin Clinton
   GTACharacter franklin = GTACharacter(name: 'Franklin Clinton', universe: 'Gran Theft Auto', mainSkill: 'Slow Motion', childrenCount: 0, isProtagonist: true, weapon: 'M16', money: 0.15, sanity: 30);
 
+  /// NPC GTA
+  GTANPCCharacter npc1 = GTANPCCharacter(name: 'Tempeny', universe: 'Gran Theft Auto', mainSkill: 'Corrupto', childrenCount: 0, isProtagonist: false, weapon: 'Spas', money: 1000000, sanity: 70, type: 'Policial', isFriendly: 'Hostil');
+
   print(scroogeMcDuck.log());
   print(itashi.print());
   print(franklin.printGTA());
+  print(npc1.printGTANPC());
 }
--- dart_orientacao/bin/npcGTA_character.dart
+++ dart_orientacao/bin/npcGTA_character.dart
@@ -1,1 +1,27 @@
-teste
+import 'gta_character.dart';
+
+class GTANPCCharacter extends GTACharacter {
+
+  /// Tipo do NPC
+  String type;
+
+  /// Se o NPC é amigável ou hostil
+  String isFriendly;
+
+  GTANPCCharacter({
+    required super.name,
+    required super.universe,
+    required super.mainSkill,
+    required super.isProtagonist,
+    required super.childrenCount,
+    required super.weapon,
+    required super.money,
+    required super.sanity,
+    required this.type,
+    required this.isFriendly,
+  });
+
+  String printGTANPC() {
+    return '${super.printGTA()}\n Tipo: $type\n Comportamento: $isFriendly';
+  }
+}
